0e7fabb2dab7197d956cfacef0c8146330f16126
Justyna Karolska <justyna.karolska@gmail.com>
2022-09-01 20:15:21 +0200

    Added one test for better coverage

--- src/test/groovy/pl/futurecollars/invoicing/service/JsonServiceTest.groovy
+++ src/test/groovy/pl/futurecollars/invoicing/service/JsonServiceTest.groovy
@@ -1,5 +1,6 @@
 package pl.futurecollars.invoicing.service
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import pl.futurecollars.invoicing.TestHelpers
 import pl.futurecollars.invoicing.model.Invoice
 import spock.lang.Specification
@@ -24,4 +25,19 @@ class JsonServiceTest extends Specification {
         invoice.getDate() == invoiceFromJson.getDate()
     }
 
+    def "return exception when parsing wrong json string"() {
+        given:
+        def jsonService = new JsonService()
+        ObjectMapper objectMapper = new ObjectMapper()
+        def invoice3 = TestHelpers.invoice(3)
+        def jsonString = objectMapper.writeValueAsString(invoice3)
+        jsonString = jsonString.replace('[', 'z')
+
+        when:
+        jsonService.stringToObject(jsonString, Invoice)
+
+        then:
+        RuntimeException exception = thrown(RuntimeException)
+        exception.message == "Serialization from string to object failed"
+    }
 }
