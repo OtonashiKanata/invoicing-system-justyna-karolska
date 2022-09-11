364d80d351d9455b450c779e82f83a682d84ee21
Justyna Karolska <justyna.karolska@gmail.com>
2022-09-11 12:28:46 +0200

    Fixed configuration

--- src/main/java/pl/futurecollars/invoicing/config/DbConfiguration.java
+++ src/main/java/pl/futurecollars/invoicing/config/DbConfiguration.java
@@ -3,11 +3,14 @@ package pl.futurecollars.invoicing.config;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import pl.futurecollars.invoicing.db.Database;
 import pl.futurecollars.invoicing.db.file.FileBasedDatabase;
 import pl.futurecollars.invoicing.db.file.FilesService;
+import pl.futurecollars.invoicing.db.memory.InMemoryDatabase;
 import pl.futurecollars.invoicing.service.IdService;
+import pl.futurecollars.invoicing.service.InvoiceService;
 import pl.futurecollars.invoicing.service.JsonService;
 
 public class DbConfiguration {
@@ -15,6 +18,26 @@ public class DbConfiguration {
   public static final String INVOICES_FILE_NAME = "invoices.txt";
   public static final String ID_FILE_NAME = "id.txt";
   public static final String DATABASE_LOCATION = "db";
+
+  @Bean
+  public JsonService jsonService() {
+    return new JsonService();
+  }
+
+  @Bean
+  public FilesService fileService() {
+    return new FilesService();
+  }
+
+  @Bean
+  public InMemoryDatabase inMemoryDatabase() {
+    return new InMemoryDatabase();
+  }
+
+  @Bean
+  public InvoiceService invoiceService(@Qualifier("fileBasedDatabase") Database database) {
+    return new InvoiceService(database);
+  }
 
   @Bean
   public IdService idService(FilesService filesService) throws IOException {
